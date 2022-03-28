91f0214583ed80d1ecc99a1aedd86763d4bb1188
nKettu <niklos.kettunen@edu.savonia.fi>
2022-03-28 17:58:20 +0300

    Users crud - alustava

--- services/Users.js
+++ services/Users.js
@@ -2,20 +2,14 @@ import User from "../models/user.js";
 
 //Toimii + testattu
 export const deleteUser = async (sightId, userId) => {
-    const user = await User.deleteOne({ _id: sightId, user:userId});
-    return user
-  };
-  
-  //Turha, hoidetaan frontin puolella
-  export const findUsers = async () => {
-    const users = await User.find({}).populate("user");
-    return users;
-  };
-  
-  //Tämä toimii
-  export const updateUser = async (userId, data) => {
-    //prettier-ignore
-    const result = await User.findOneAndUpdate({ _id: userId}, data, { new:true })
-    if (!result) throw new Error("Not Found");
-    return result;
-  };
+  const user = await User.deleteOne({ _id: sightId, user: userId });
+  return user;
+};
+
+//Tämä toimii
+export const updateUser = async (userId, data) => {
+  //prettier-ignore
+  const result = await User.findOneAndUpdate({ _id: userId}, data, { new:true })
+  if (!result) throw new Error("Not Found");
+  return result;
+};
