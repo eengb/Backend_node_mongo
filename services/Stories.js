badbcee7b3b091fd3d7a37e65dfa4a2baa5cede6
Esa Engblom <esa.engblom@gmail.com>
2022-03-28 18:46:33 +0300

    siivous

--- services/Stories.js
+++ services/Stories.js
@@ -1,18 +1,14 @@
 import Story from "../models/story.js";
 
 
-//Toimii + testattu
 export const deleteStory = async (storyId, userId) => {
     const story = await Story.deleteOne({ _id: storyId, user:userId});
     return story
   };
   
   
-
-  
-  //Tämä toimii
   export const updateStory = async (storyId, userId, data) => {
-    //prettier-ignore
+  
     const result = await Story.findOneAndUpdate({ _id: storyId, user:userId }, data, { new:true })
     if (!result) throw new Error("Not Found");
     return result;
